5cdcbebf489dd045eb804a8221133eaffa2dd44f
Stefano <michste93@gmail.com>
2015-07-21 20:09:04 +0200

    german finished

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
 jQuery(document).ready(function($){
   
-var content = document.getElementsById('scrollDiv');
-content.addEventListener('touchstart', function (event) {
-    this.allowUp = (this.scrollTop > 0);
-    this.allowDown = (this.scrollTop < this.scrollHeight - this.clientHeight);
-    this.slideBeginY = event.pageY;
-});
 
-content.addEventListener('touchmove', function (event) {
-    var up = (event.pageY > this.slideBeginY);
-    var down = (event.pageY < this.slideBeginY);
-    this.slideBeginY = event.pageY;
-    if ((up && this.allowUp) || (down && this.allowDown)) {
-        event.stopPropagation();
-    }
-    else {
-        event.preventDefault();
-    }
-});
     
     
    
