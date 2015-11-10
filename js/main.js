a4ddb61388d7ee5586b442a3cbd015d5fae230e9
Stefano <michste93@gmail.com>
2015-11-10 11:39:06 +0100

    new new

--- js/main.js
+++ js/main.js
@@ -9,24 +9,7 @@ jQuery(document).ready(function($){
   	
 //  	});
     
-    
-        
-    $("#contact form").submit(function(){
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var object = $("#object").val();
-        var mex = $("textarea").val();
-        var file = "name="+name+"&email="+email+"&object="+object+"&mex="+mex;
-        $.post("../php/contact.php",file,function(e){
-	        alert(e);
-        	$("#contact").hide(300);
-        	email="";
-        	object="";
-        	mex="";
-        });
-        return false;
-    });
-	 
+     
 	$('.cd-primary-nav').on('click', function(event){
 		if($(event.target).is('.cd-primary-nav')) 
 		   $(this).children('ul').toggleClass('is-visible');
